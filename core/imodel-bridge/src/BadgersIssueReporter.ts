248e8392ce909a33dca01806b5ee24927ba1110a
akshay.madhukar <46531236+akshay-madhukar@users.noreply.github.com>
2021-06-07 17:26:42 -0400

    Cleans unused variables

--- core/imodel-bridge/src/BadgersIssueReporter.ts
+++ core/imodel-bridge/src/BadgersIssueReporter.ts
@@ -2,19 +2,19 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-import { DbResult, Guid, GuidString } from "@bentley/bentleyjs-core";
+import { DbResult, Guid, GuidString, Logger } from "@bentley/bentleyjs-core";
 import { ECDb } from "@bentley/imodeljs-backend";
 import { BridgeIssueReporter } from "./BridgeIssueReporter"
 import * as fs from "fs";
 import * as path from "path";
 import * as os from "os";
-import { fips } from "crypto";
+import { BridgeLoggerCategory } from "./BridgeLoggerCategory";
 
 interface ContextInfo {
   activityId: string;
   contextId: string;
   iModelId: string;
-  briefcaseId: number;
+  briefcaseId?: number;
   operationType?: string;
   jobId?: string;
 }
@@ -30,45 +30,36 @@ interface FileInfo {
 export class BadgersIssueReporter implements BridgeIssueReporter {
   private _reportDb;
   private _reportFile;
-  private _reportSummaryFile; //ToDo create summary report
   private _stagingDir;
   private _contextInfo: ContextInfo;
   private _fileInfo: FileInfo;
   private _dbPath: string = "";
-  // private _authHandler; //Callback to get token. Refer to serverArgs
-  // private _signInMgr;
-  // private _isValidContext;
   private _deleteBadgersDb;
-  private _categoryCaluse;
-  private _typeCaluse;
-  private _levelCaluse;
 
   private TEMP_REPORT_FILE_RECORDS_TABLE = "BadgersReportFileRecords";
   private TEMP_REPORT_AUDIT_RECORDS_TABLE = "BadgersReportAuditRecords";
   private SRCFILE_TEMP_BRIDGE_ISSUES_TABLE = "BadgersSrcFileReports";
   private TEMP_IGNORED_ELEM_RECORD_TABLE = "BadgersReportIgnoredElementRecords";
 
-  public constructor(contextId: any, iModelId: GuidString, activityId: string, fileId: string, operationType?: string, filePath?: any,
-    stagingDir?: any, jobId?: string, deleteReportDB?: boolean) {
+  public constructor(contextId: string, iModelId: GuidString, activityId: string, fileId: string, stagingDir?: any, operationType?: string,
+    filePath?: any, jobId?: string, deleteReportDB?: boolean) {
 
-    const opType = operationType ? operationType : "NORMAL_UPDATE";
-    this._contextInfo = { operationType: opType, activityId, jobId, contextId, iModelId, briefcaseId: 2 };
+    const opType = operationType ?? "NORMAL_UPDATE";
+    this._contextInfo = { operationType: opType, activityId, jobId, contextId, iModelId };
     this._fileInfo = { fileId, filePath };
-    this._stagingDir = stagingDir ? stagingDir : os.tmpdir();
-    this._deleteBadgersDb = deleteReportDB ? deleteReportDB : false;
-
-    this._categoryCaluse = "'Business Properties'";
-    this._typeCaluse = "'Model', 'ProjectExtents'";
-    this._levelCaluse = "'Error', 'Warning'";
+    this._stagingDir = stagingDir ?? os.tmpdir();
+    this._deleteBadgersDb = deleteReportDB ?? false;
 
     this._reportFile = this.computeReportFileName(activityId, false);
-    this._reportSummaryFile = this.computeReportFileName(activityId, true);
 
     this._reportDb = new ECDb();
     this.createBadgersDb();
     this.createDBSchema();
   }
 
+  public setBriefcaseId(briefcaseId: number) {
+    this._contextInfo.briefcaseId = briefcaseId;
+  }
 
   private computeReportFileName(activityId: string, isSummary: boolean) {
     let reportFileName = `${activityId}-${this._contextInfo.operationType}`
@@ -102,7 +93,9 @@ export class BadgersIssueReporter implements BridgeIssueReporter {
   }
 
   public recordSourceFileInfo(srcId: string, name: string, uniqueName: string, itemType: string, dataSource: string, state: string, failureReason: string, exists: boolean, fileSize: number, foundByBridge: boolean, downloadUrl?: string,) {
-    //If the id is not the same throw a warning
+    if (srcId !== this._fileInfo.fileId) {
+      Logger.logWarning(BridgeLoggerCategory.Framework, "Source file Id does not match value set in constructor");
+    }
     const values = [this._contextInfo.activityId, this._contextInfo.contextId, this._contextInfo.jobId, this._contextInfo.iModelId, this._contextInfo.briefcaseId, srcId, this._fileInfo.filePath,
       "SourceFile", itemType, dataSource, name, exists, downloadUrl, state, failureReason, uniqueName, fileSize, foundByBridge]
     this._reportDb.withPreparedSqliteStatement(`INSERT INTO ${this.SRCFILE_TEMP_BRIDGE_ISSUES_TABLE}(activityId, contextId, jobId, imodelId, briefcaseId, fileId, filePath, dataType, itemType, dataSource, fileName, itemExists, downloadUrl, state, failureReason, uniqueName, fileSize, foundByBridge) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`, (stmt) => {
@@ -123,14 +116,14 @@ export class BadgersIssueReporter implements BridgeIssueReporter {
   public createJsonReport() {
     const timestamp = new Date().toISOString();
     const context = {
-      "reportType": "detailReport",
-      "operationType": this._contextInfo.operationType,
-      "jobid": this._contextInfo.jobId,
-      "contextid": this._contextInfo.contextId,
-      "imodelid": this._contextInfo.iModelId,
-      "activityid": this._contextInfo.activityId,
-      "briefcaseid": this._contextInfo.briefcaseId,
-      "timestamp": timestamp
+      reportType: "detailReport",
+      operationType: this._contextInfo.operationType,
+      jobid: this._contextInfo.jobId,
+      contextid: this._contextInfo.contextId,
+      imodelid: this._contextInfo.iModelId,
+      activityid: this._contextInfo.activityId,
+      briefcaseid: this._contextInfo.briefcaseId,
+      timestamp: timestamp
     };
 
     const sourceFile: any = {};
@@ -158,13 +151,13 @@ export class BadgersIssueReporter implements BridgeIssueReporter {
       while (stmt.step() === DbResult.BE_SQLITE_ROW) {
         const row = stmt.getRow()
         referenceFiles.push({
-          "fileId": row.fileId,
-          "fileName": row.fileName,
-          "path": row.filePath,
-          "state": row.state,
-          "iModelFileId": row.uniqueName,
-          "fileExists": row.itemExists,
-          "bimFileExists": true //ToDo query iModel
+          fileId: row.fileId,
+          fileName: row.fileName,
+          path: row.filePath,
+          state: row.state,
+          iModelFileId: row.uniqueName,
+          fileExists: row.itemExists,
+          bimFileExists: true //ToDo query iModel
         })
       }
     });
@@ -176,15 +169,15 @@ export class BadgersIssueReporter implements BridgeIssueReporter {
       while (stmt.step() === DbResult.BE_SQLITE_ROW) {
         const row = stmt.getRow();
         auditrecords.push({
-          "elementinfo": {
-            "ecinstanceid": row.ecInstanceId,
-            "sourceid": row.elementSourceId
+          elementinfo: {
+            ecinstanceid: row.ecInstanceId,
+            sourceid: row.elementSourceId
           },
-          "auditinfo": {
-            "level": row.auditLevel,
-            "category": row.auditCategory,
-            "message": row.auditMessage,
-            "type": row.auditType
+          auditinfo: {
+            level: row.auditLevel,
+            category: row.auditCategory,
+            message: row.auditMessage,
+            type: row.auditType
           }
         })
       }
@@ -195,27 +188,27 @@ export class BadgersIssueReporter implements BridgeIssueReporter {
       while (stmt.step() === DbResult.BE_SQLITE_ROW) {
         const row = stmt.getRow();
         ignoredelements.push({
-          "ignoredelementinfo": {
-            "repositorylinkId": row.repositoryId,
-            "elementsids": row.elementIds
+          ignoredelementinfo: {
+            repositorylinkId: row.repositoryId,
+            elementsids: row.elementIds
           }
         })
       }
     });
 
     const fileRecord: any[] = [{
-      "file": {
-        "identifier": sourceFile.fileId,
-        "path": sourceFile.path,
-        "ignoredelements": ignoredelements,
+      file: {
+        identifier: sourceFile.fileId,
+        path: sourceFile.path,
+        ignoredelements: ignoredelements,
       },
-      "auditrecords": auditrecords
+      auditrecords: auditrecords
     }];
 
     const report = {
-      "context": context,
-      "sourceFilesInfo": sourceFile,
-      "filerecords": fileRecord
+      context: context,
+      sourceFilesInfo: sourceFile,
+      filerecords: fileRecord
     }
     const reportFilePath = path.join(this._stagingDir, this._reportFile);
     fs.writeFileSync(reportFilePath, JSON.stringify(report));
@@ -252,10 +245,10 @@ export class BadgersIssueReporter implements BridgeIssueReporter {
     this._reportDb.withSqliteStatement(`CREATE INDEX ${this.TEMP_REPORT_FILE_RECORDS_TABLE}ActivityIdx ON ${this.TEMP_REPORT_FILE_RECORDS_TABLE}(activityId)`, (stmt) => { stmt.step(); });
 
     this._reportDb.withSqliteStatement(`CREATE TABLE ${this.TEMP_REPORT_AUDIT_RECORDS_TABLE}(fileId STRING NOT NULL, ecInstanceId STRING, elementSourceId STRING, auditLevel STRING,
-            auditCategory STRING, auditMessage STRING, auditType STRING, GUID STRING)`, (stmt) => { stmt.step(); });
+			auditCategory STRING, auditMessage STRING, auditType STRING, GUID STRING)`, (stmt) => { stmt.step(); });
 
     this._reportDb.withSqliteStatement(`CREATE TABLE ${this.SRCFILE_TEMP_BRIDGE_ISSUES_TABLE}(activityId STRING, contextId STRING, jobId STRING, imodelId STRING, briefcaseId STRING,
-                fileId STRING, filePath STRING, dataType STRING, itemType STRING, dataSource STRING, fileName STRING, itemExists BOOLEAN, downloadUrl STRING, state STRING, failureReason STRING, uniqueName STRING, fileSize INTEGER, foundByBridge BOOLEAN)`, (stmt) => { stmt.step(); });
+				fileId STRING, filePath STRING, dataType STRING, itemType STRING, dataSource STRING, fileName STRING, itemExists BOOLEAN, downloadUrl STRING, state STRING, failureReason STRING, uniqueName STRING, fileSize INTEGER, foundByBridge BOOLEAN)`, (stmt) => { stmt.step(); });
     this._reportDb.withSqliteStatement(`CREATE INDEX ${this.SRCFILE_TEMP_BRIDGE_ISSUES_TABLE}ActivityIdx ON ${this.SRCFILE_TEMP_BRIDGE_ISSUES_TABLE}(activityId)`, (stmt) => { stmt.step(); });
 
     this._reportDb.withSqliteStatement(`CREATE TABLE ${this.TEMP_IGNORED_ELEM_RECORD_TABLE}(repositoryId STRING, elementIds STRING)`, (stmt) => { stmt.step(); });
@@ -281,7 +274,6 @@ export class BadgersIssueReporter implements BridgeIssueReporter {
 
   public publishReport() {
     this.createJsonReport();
-    //Upload report file
     this.deleteBadgersDb();
   }
 
